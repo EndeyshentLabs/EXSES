60f8c0521f69fe5a7b16d24b7a393c0c55595df8
EndeyshentLabs <Themikfound@gmail.com>
2023-05-19 16:00:18 +0300

    [main.cpp] Cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,7 @@
-#include "Lexer.hpp"
+#include <Lexer.hpp>
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <vector>
-#include <cassert>
-
-#include <utils.hpp>
-#include <Token.hpp>
-
-#define push(prg, nu) do { prg.push_back(Token(PUSH, nu)); } while (0)
-#define plus(prg) do { prg.push_back(Token(PLUS)); } while (0)
-#define minus(prg) do { prg.push_back(Token(MINUS)); } while (0)
-#define mult(prg) do { prg.push_back(Token(MULT)); } while (0)
-#define div(prg) do { prg.push_back(Token(DIV)); } while (0)
-#define dup(prg) do { prg.push_back(Token(DUP)); } while (0)
-#define swap(prg) do { prg.push_back(Token(SWAP)); } while (0)
-#define dump(prg) do { prg.push_back(Token(DUMP)); } while (0)
 
 #if 0
 void convertToPython3(std::vector<Token> program)
@@ -65,23 +51,10 @@ int main(int argc, char* argv[])
     std::ifstream sourceFile;
     std::string sourceFileContents;
 
-    // std::vector<Token> prg;
-    // push(prg, 123);
-    // push(prg, 7);
-    // plus(prg);
-    // push(prg, 30);
-    // minus(prg);
-    // dump(prg);
-    // push(prg, 3);
-    // dup(prg);
-    // mult(prg);
-    // dump(prg);
-
     if (argc < 2) {
         std::cerr << "ERROR: Not enough arguments is provided!\n";
         return 1;
     }
-
 
     sourceFile.open(argv[1], std::ios::in);
     if (sourceFile.is_open()) {
@@ -98,12 +71,6 @@ int main(int argc, char* argv[])
         std::cerr << "ERROR: Couldn't open a file '" << argv[1] << "'!\n";
         return 1;
     }
-
-    // std::vector<std::string> words = split(sourceFileContents, "\\s");
-
-    // for (const auto& word : words) {
-    //     std::cout << word << '\n';
-    // }
 
     return 0;
 }
